6c5cd9671c369d76d2446680e8e83f474fd904e0
Marco Boccenti <marco.boccenti@gmail.com>
2022-11-27 11:21:01 +0100

    Update GoToCubicle.cs

--- Assets/GoToCubicle.cs
+++ Assets/GoToCubicle.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Go to cubicle action for the nurse
+/// </summary>
 public class GoToCubicle : GAction
 {
     public override bool PrePerform()
@@ -14,10 +17,18 @@ public class GoToCubicle : GAction
 
     public override bool PostPerform()
     {
+        // Add one to the "TreatingPatient" world state 
         GWorld.Instance.GetWorld().ModifyState("TreatingPatient", 1);
+        
+        // Give back one of the cubicle
         GWorld.Instance.AddCubicle(target);
+        
+        // Remove the cubicle from the nurse's inventory
         inventory.RemoveItem(target);
+        
+        // Add one to the "FreeCubicle" world state
         GWorld.Instance.GetWorld().ModifyState("FreeCubicle", 1);
+        
         return true;
     }
 }
